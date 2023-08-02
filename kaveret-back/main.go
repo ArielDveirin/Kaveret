99e5101e52d2f75d002b41df30025e1c2f32afa2
ArielDveirin <ariel.private2@gmail.com>
2023-08-02 14:21:36 +0300

    added json parser in the backend

--- kaveret-back/main.go
+++ kaveret-back/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
+
+type RegisterUser struct {
+	Username string
+	Password string
+	Email    string
+}
 
 func postRegisterDetails(c *gin.Context) {
 	jsonData, err := ioutil.ReadAll(c.Request.Body)
@@ -16,8 +23,18 @@ func postRegisterDetails(c *gin.Context) {
 	c.JSON(200, gin.H{
 		string(jsonData): "Hello!",
 	})
+	var user RegisterUser
 
-	fmt.Println("got username: ", string(jsonData))
+	parseErr := json.Unmarshal(jsonData, &user)
+
+	if parseErr != nil {
+
+		// if error is not nil
+		// print error
+		fmt.Println(parseErr)
+	}
+
+	fmt.Printf("Username: %s\nPassword: %s\nEmail: %s\n", user.Username, user.Password, user.Email)
 
 }
 
